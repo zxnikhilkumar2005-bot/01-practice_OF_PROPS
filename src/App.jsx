922afb09bc9d600402f88d73934584dc1f0b2684
Nikhil kumar <zxnihilkumar2005@gmail.com>
2026-01-22 11:00:37 +0530

    All chages done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,95 @@ import React from 'react'
 import Cards from './components/Cards'
 
 const App = () => {
-  return (
-    <div>
-      <Cards 
+  const cards_data = [
+    {
+      "rarity": "Uncommon",
+      "price": "0.5 ETH",
+      "title": "HAPE #67",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$5/h",
+      "distribution": {
+        "green": 80,
+        "purple": 20
+      },
+      "imagePrompt": "https://i.pinimg.com/736x/61/1d/74/611d7498b4a2c9bf53256fe4c23e2d89.jpg"
+    },
+    {
+      "rarity": "Uncommon",
+      "price": "0.6 ETH",
+      "title": "HAPE #67",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$5/h",
+      "distribution": {
+        "green": 80,
+        "purple": 20
+      },
+      "imagePrompt": "https://i.pinimg.com/736x/d8/80/77/d88077b52cf7f73d05a0ec6d6404f8d6.jpg"
+    },
+    {
+      "rarity": "Rare",
+      "price": "1.2 ETH",
+      "title": "HAPE #102",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$9/h",
+      "distribution": {
+        "green": 65,
+        "purple": 35
+      },
+      "imagePrompt": "https://i.pinimg.com/736x/13/03/79/130379b45684200933285589490d2984.jpg"
+    },
+    {
+      "rarity": "Epic",
+      "price": "2.8 ETH",
+      "title": "HAPE #245",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$15/h",
+      "distribution": {
+        "green": 50,
+        "purple": 50
+      },
+      "imagePrompt": "https://i.pinimg.com/736x/28/35/77/2835772b4161e52e9cc2553fb703f7e3.jpg"
+    },
+    {
+      "rarity": "Legendary",
+      "price": "5.0 ETH",
+      "title": "HAPE #001",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$30/h",
+      "distribution": {
+        "green": 30,
+        "purple": 70
+      },
+      "imagePrompt": "https://i.pinimg.com/1200x/86/7a/f5/867af5571912376eaedeee1a223917e9.jpg"
+    },
+    {
+      "rarity": "Common",
+      "price": "0.2 ETH",
+      "title": "HAPE #389",
+      "collection": "HAPE PRIME",
+      "avgEarnings": "$2/h",
+      "distribution": {
+        "green": 90,
+        "purple": 10
+      },
+      "imagePrompt": "https://i.pinimg.com/1200x/95/78/02/9578028cc422d17a16bda3e2347fb692.jpg"
+    },
 
-      
-      />
+  ]
+  return (
+    <div class="cards-container">
+      {cards_data.map(function(elem,idex){
+        return <div>
+          <Cards
+          imagePrompt={elem.imagePrompt}
+          price={elem.price}
+          title={elem.title}
+          collection={elem.collection}
+          avgEarnings={elem.avgEarnings}
+          distribution={elem.distribution}
+          />
+        </div>
+      })}
     </div>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,42 +1,36 @@
 import React from 'react'
 
-const Cards = () => {
-    return (
-<div class="cards-container">
+const Cards = (props) => {
+  return (
+    <div class="nft-card">
 
-  <div class="nft-card">
-    <div class="image-section">
-
-      <img src="https://i.pinimg.com/736x/80/0c/0d/800c0da79c5963e0855b4c6b4790babd.jpg" alt="" />
-      <span class="price">0.5 ETH</span>
-    </div>
-
-    <div class="content">
-      <h2>HAPE #67</h2>
-      <div class="brand">
-        <span class="logo">H</span>
-        HAPE PRIME
+      <div class="image-section">
+        <img src={props.imagePrompt} alt="" />
+        <span class="price">{props.price}</span>
       </div>
-
-      <div class="stats">
-        <div>
-          <p class="label">Avg earnings</p>
-          <p class="value">$5/h</p>
+      <div class="content">
+        <h2>{props.title}</h2>
+        <div class="brand">
+          <span class="logo">H</span>
+          {props.collection}
         </div>
-        <div>
-          <p class="label">Distribution %</p>
-          <div class="dist">
-            <span class="dot green"></span>80
-            <span class="divider"></span>
-            20<span class="dot purple"></span>
+        <div class="stats">
+          <div>
+            <p class="label">{props.avgEarnings}</p>
+            <p class="value">{props.avgEarnings}</p>
+          </div>
+          <div>
+            <p class="label">Distribution %</p>
+            <div class="dist">
+              <span class="dot green"></span>{props.distribution.green}
+              <span class="divider"></span>
+              {props.distribution.purple}<span class="dot purple"></span>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-    )
+  )
 }
 
 export default Cards
